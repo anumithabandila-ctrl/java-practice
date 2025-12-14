40306a5f89c3f0b8916072e2804ae7731cf983a5
BANDILA ANUMITHA <anumithabandila@gmail.com>
2025-12-14 19:26:06 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int temp = num, rev = 0;
+
+        while (num != 0) {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+
+        if (temp == rev)
+            System.out.println(temp + " is a Palindrome");
+        else
+            System.out.println(temp + " is NOT a Palindrome");
+    }
+}
